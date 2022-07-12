067985bfdcfa45422e2b04d9580d04e75c8e8dbb
george <ge_silva1991@hotmail.com>
2022-07-12 17:22:20 -0300

    Add rolar dados

--- app/jogo.py
+++ app/jogo.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
-from random import sample
-from typing import List
+from random import randint, sample
 
 
 @dataclass
@@ -8,8 +7,12 @@ class Jogo:
     lista_jogagores = ["azul", "branco", "vermelho", "preto"]
     lista_jogagores_restantes = lista_jogagores
     rodada: int = 0
+    dado_resultado: int = None
 
     def ordenar_jogadores(self):
         self.lista_jogagores_restantes = sample(
             self.lista_jogagores, k=len(self.lista_jogagores)
         )
+
+    def rolar_dado(self):
+        self.dado_resultado = randint(1, 6)
